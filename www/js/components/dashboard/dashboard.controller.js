356dc326431c5e927eb7d81168b80e5fbd00e5af
Alon Rosenfeld <alon.rosenfeld@weissbeerger.com>
2018-03-09 17:27:55 +0200

    init structure for dashboard and new game form

--- www/js/components/dashboard/dashboard.controller.js
+++ www/js/components/dashboard/dashboard.controller.js
@@ -1,7 +1,29 @@
 angular.module('dartsApp.dashboardCtrl', [])
 
     .controller('dashboardCtrl', function ($scope) {
-        $scope.test = "controller works"
+
+        $scope.$on('$ionicView.enter', function(){
+            console.warn('dashboard CTLR on Enter...');
+
+            $scope.showGameSelectionList = false;
+        });
+
+        // show the new game list options
+        $scope.newGameSelectionClick = function () {
+            $scope.showGameSelectionList = true;
+        };
+
+        $scope.numberOfPlayers = {
+            1: "One player",
+            2: "Two players",
+            3: "Three players",
+            4: "Four players",
+            5: "Five players"
+        }
+
+        // todo: add score limit, number of throws to options
+        // todo: gather all the selections in ng-model form and submit to local storage
+
     });
 
 
